76451d1883f0ff88658ac29a66d076e4ddb1407c
ValeruMounika <105640236+ValeruMounika@users.noreply.github.com>
2023-02-20 10:40:13 +0530

    dropdown data adding in updateEmployee component

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent {
+  Shifts:string[];
+  roles:string[];
+  constructor(){
 
+  }
+  ngOnInit(){
+    this.Shifts=["selectShift","FirstShift","SecondShift"];
+    this.roles=[ "Accountant",
+    "InVoiceSpecialist",
+     "HRExecutive",
+     "QALead",
+     "SoftWareDeveloper",
+     "SoftwareEngineer",
+     "SoftwareTrainee",
+    "QALead",
+     "QA Trainee",
+     "Tech Lead",
+    "UI Designer",
+     "Sr.SoftwareDeveloper",
+    "Project Manager",
+     "Sr.SoftwareEngineer",
+     "Sr.QA"
+    ];
+  }
 }
